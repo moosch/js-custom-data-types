1ca3d072758d38d1f23f5df1e0e4dc9683b6b3d2
moosch <mail@moosch.io>
2019-11-26 21:40:29 +1030

    Adds add fn

--- Box.js
+++ Box.js
@@ -17,6 +17,13 @@ const _boxPeek = (datatype) => {
   console.log(`Box(${datatype.valueOf()})`);
   return datatype;
 };
+const _boxAdd = (data, val) => {
+  typeGuard(Box, data);
+  if (isNaN(val) && typeof val !== 'string') {
+    throw new TypeError(`Expected either a string or number type but got ${typeof val}`);
+  }
+  return Box(`${data.valueOf()}${val}`);
+};
 const _boxOpen = (datatype) => {
   typeGuard(Box, datatype);
   return datatype.valueOf();
@@ -27,12 +34,14 @@ const Box = (x) => ({
   valueOf: () => x,
   shake: () => _boxShake(Box(x)),
   peek: () => _boxPeek(Box(x)),
+  add: (val) => _boxAdd(Box(x), val),
   open: () => _boxOpen(Box(x)),
 });
 
 Object.defineProperties(Box, {
   shake: { value: _boxShake },
   peek: { value: _boxPeek },
+  add: { value: _boxAdd },
   open: { value: _boxOpen },
 });
 
